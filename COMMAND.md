4c6c5e030610e2e344132e3bb6cbe9b9bc18343d
vol-mir <volmirs45@gmail.com>
2022-06-12 17:59:18 +0300

    Update database settings project

--- COMMAND.md
+++ COMMAND.md
@@ -1,7 +1,8 @@
 # USE COMMAND
 
 ## UNIX
-chmod -R 777 /var/www - add 777 access rights
+chmod -R 777 /var/www        - add 777 access rights
+rm Version20220612133652.php - delete file
 
 ## Docker
 sudo docker kill $(sudo docker ps -q) - stop all container
--- app/deploy.sh
+++ app/deploy.sh
@@ -32,7 +32,6 @@ while true; do
             rm -r migrations/*
             mkdir migrations
             php bin/console doctrine:database:create --if-not-exists
-            # php bin/console doctrine:schema:drop --force --full-database
             php bin/console doctrine:migration:diff
             php bin/console doctrine:migration:migrate --no-interaction
             

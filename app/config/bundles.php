eb75d8643e204010b42acd202930f2c40cb86ade
vol-mir <vladimir.bykovski@tut.by>
2022-06-19 15:26:21 +0300

    Добавление фикстур

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
